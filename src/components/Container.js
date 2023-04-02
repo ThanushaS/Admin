f42fad5ea5a8a5813cf41771d9aea5d5d5978925
ThanushaS <126234282+ThanushaS@users.noreply.github.com>
2023-04-02 19:58:29 +0530

    changes

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './container.css'
 const Container = () => {
     return (
-        <div>
-            <h1></h1>
-            <div class="container-fluid px-4">
+        <div>           
+             <div class="container-fluid px-5">
                 <div class="row g-3 my-0">
                     <div class="col-md-3">
                         <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,42 +11,48 @@ const Navbar = () => {
     const[inactive, setInactive]= useState(false);
   
   return (
-        <div className="navbar navbar-expand-lg navbar-light  py-4 px-4">
+    <div className={`navbar navbar-expand-lg navbar-light  py-4 px- ${inactive?"inactive":""}`}>
             <div>
-              <div class="d-flex align-items-center">
-                <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
+              <div class=" item d-flex align-items-center">
+                <i onClick={()=>{setInactive(!inactive);}} class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
               </div>
-            </div>
-                <div className="search-controller">
+              </div>
+              <div className="search-controller">
                   <button className="search-btn">
                     <i class="bi bi-search"></i>
                   </button>
                 <input type="text" placeholder="Search..." />
-                </div>
-          <nav>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"aria-expanded="false" aria-label="Toggle navigation">
+              </div>
+      <nav>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <div className='items'>
                 <div className="item">
-                    <MailOutlineOutlinedIcon className="icon"/>  
-                    <div className='counter'>3</div>            
+                    <i class="bi bi-envelope-fill"></i>
+                    <div className='counter'></div>            
                 </div>
                 <div className="item">
-                    <NotificationsNoneOutlinedIcon className="icon"/> 
-                    <div className='counter'>2</div>             
+                    <i class="bi bi-bell-fill"></i>
+                    <div className='counter'></div>             
                 </div>  
                 <div className="item">
-                    <NotificationsNoneOutlinedIcon className="icon"/> 
-                <div className='counter'>2</div>             
+                    <i class="bi bi-gear-fill"></i>
+                </div>
+                <div className="item">
+                  <img src={user} alt="user" className="avatar"/>
+        <div class="name-job">
+          <div class="profile_name">John</div>
+          <div class="job">cod@gmail.com</div>
+        </div>
                 </div>
                 </div>
               </ul>
-            </div>
-          </nav>
         </div>
+      </nav>
+    </div>
           
   );
 };
