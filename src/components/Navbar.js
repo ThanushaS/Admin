d29c6e65eddd85be22fe10a7a29008096afd5977
ThanushaS <126234282+ThanushaS@users.noreply.github.com>
2023-04-04 16:52:52 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,59 @@
 import './navbar.css';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
-
 //import Logo from "../assets/Logo/logo.jpg";
 import user from "../assets/user.jpg";
-
 import { useState, useEffect } from "react";
-
+//import {useRef} from "react";
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const Navbar = () => {
-    const[inactive, setInactive]= useState(false);
+  const[isMobile, setIsMobile]=useState(false);
+
+
+
   
   return (
-    <div className={`navbar navbar-expand-lg navbar-light  py-4 px- ${inactive?"inactive":""}`}>
-            <div>
-              <div class=" item d-flex align-items-center">
-                <i onClick={()=>{setInactive(!inactive);}} class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-              </div>
-              </div>
-              <div className="search-controller">
-                  <button className="search-btn">
-                    <i class="bi bi-search"></i>
-                  </button>
-                <input type="text" placeholder="Search..." />
-              </div>
-      <nav>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <div className='items'>
-                <div className="item">
-                    <i class="bi bi-envelope-fill"></i>
-                    <div className='counter'></div>            
-                </div>
-                <div className="item">
-                    <i class="bi bi-bell-fill"></i>
-                    <div className='counter'></div>             
-                </div>  
-                <div className="item">
-                    <i class="bi bi-gear-fill"></i>
-                </div>
-                <div className="item">
-                  <img src={user} alt="user" className="avatar"/>
-        <div class="name-job">
-          <div class="profile_name">John</div>
-          <div class="job">cod@gmail.com</div>
+    <div className='navbar navbar-expand-lg navbar-light  py-4 px-0 '>
+        <div>
+            <div className=" item d-flex align-items-center">
+                  <i className="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle" ></i>
+            </div>
         </div>
+        <div className="search-controller">
+            <button className="search-btn">
+                <i class="bi bi-search" ></i>
+            </button>
+                  <input type="text" placeholder="Search..." />
+        </div>
+        <nav>
+        <button className="navbar-toggler" >
+          {isMobile ? (
+             <i className='fas fa-times'></i>):(<i className='fas fa-bars'></i>)}
+          </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className={isMobile ? "navbar-nav-mobile" : "navbar-nav"}>
+              <div className='items'>
+                <div className="item">
+                  <i className="bi bi-envelope-fill"></i>
+                  <div className='counter'></div>            
                 </div>
-                </div>
+                <div className="item">
+                  <i className="bi bi-bell-fill"></i>
+                  <div className='counter'></div>             
+                </div>  
+                  <div className="item">
+                      <i className="bi bi-gear-fill"></i>
+                  </div>
+                  <div className="item">
+                    <img src={user} alt="user" className="avatar"/>
+                    <div className="name-job">
+                      <div class="profile_name">John</div>
+                      <div className="mail">cod@gmail.com</div>
+                    </div>
+                  </div>
+                  </div>
               </ul>
         </div>
+        
       </nav>
     </div>
           
